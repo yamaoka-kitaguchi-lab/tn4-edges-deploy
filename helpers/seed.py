f57a04aa2f1c971c9d022db4e478dd89b300259a
mi2428 <mi2428782020@gmail.com>
2021-07-11 01:19:37 +0900

    Create VLANs
    Signed-off-by: mi2428 <mi2428782020@gmail.com>

--- helpers/seed.py
+++ helpers/seed.py
@@ -24,22 +24,51 @@ VAULT_PASSWORD_FILE = os.path.join(os.path.dirname(__file__), "../.secrets/vault
 
 class NetBoxClient:
   def __init__(self, netbox_url, netbox_api_token):
-    self.api_endpoint = "{}/api".format(netbox_url.rstrip("/"))
+    self.api_endpoint = netbox_url.rstrip("/") + "/api"
     self.token = netbox_api_token
 
-  def query(self, request_path):
+  def query(self, request_path, data=None):
     headers = {
-      "Authorization": "Token {}".format(self.token),
-      "Content-Type": "application/json"
+      "Authorization": f"Token {self.token}",
+      "Content-Type": "application/json",
+      "Accept": "application/json; indent=4"
     }
     responses = []
     url = self.api_endpoint + request_path
-    while url:
-      raw = requests.get(url, headers=headers, verify=True)
-      res = json.loads(raw.text)
-      responses += res["results"]
-      url = res["next"]
+    if data:
+      cnt, limit = 0, 100
+      while cnt < len(data):
+        d = data[cnt:cnt+limit]
+        raw = requests.post(url, json.dumps(d), headers=headers, verify=True)
+        responses += json.loads(raw.text)
+        cnt += limit
+    else:
+      while url:
+        raw = requests.get(url, headers=headers, verify=True)
+        res = json.loads(raw.text)
+        responses += res["results"]
+        url = res["next"]
     return responses
+  
+  def get_all_vlans(self):
+    return self.query("/ipam/vlans/")
+  
+  def get_all_vids(self):
+    vlans = self.get_all_vlans()
+    return [vlan["vid"] for vlan in vlans]
+  
+  def create_vlans(self, vlans):
+    existed_vids = self.get_all_vids()
+    data = [
+      {"vid": vid, "name": prop["name"], "status": "active", "description": prop["description"]}
+      for vid, prop in vlans.items() if vid not in existed_vids
+    ]
+    print("[I] {} VLANs are newly created.".format(len(data)))
+    return self.query("/ipam/vlans/", data)
+    
+  
+  def create_devices(self, devices):
+    pass
 
 
 def __load_encrypted_secrets():
@@ -57,6 +86,7 @@ def __load_encrypted_secrets():
 def main():
   secrets = __load_encrypted_secrets()
   nb = NetBoxClient(secrets["netbox_url"], secrets["netbox_api_token"])
+  nb.create_vlans(vlan_load())
 
 
 if __name__ == "__main__":
