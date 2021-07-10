9e0d4e91e9ad594ee65761d4c45a370a9c4a0a7b
mi2428 <mi2428782020@gmail.com>
2021-07-10 17:24:57 +0900

    Update
    Signed-off-by: mi2428 <mi2428782020@gmail.com>

--- helpers/batfish.py
+++ helpers/batfish.py
@@ -8,5 +8,11 @@ from pprint import pprint
 load_questions()
 bf_init_snapshot("./tn3")
 
-props = bfq.interfaceProperties(nodes="minami3-1").answer()
-pprint(props.rows)
+q = bfq.interfaceProperties(
+    nodes="minami3-1", interfaces="/ge-[0,1]\/0\/[0-9]*\.0/",
+    properties="Switchport_Mode,Access_VLAN,Allowed_VLANs,Description")
+#pprint(q.answer().rows)
+
+q = bfq.switchedVlanProperties(
+    nodes="minami3-1", interfaces="/ge-[0,1]\/0\/[0-9]*\.0/")
+pprint(q.answer().rows)
