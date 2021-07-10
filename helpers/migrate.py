cea171cbe885aef5ad0583d09732aba1c02412fb
mi2428 <mi2428782020@gmail.com>
2021-07-10 23:34:46 +0900

    Add minami3 migration rule
    Signed-off-by: mi2428 <mi2428782020@gmail.com>

---
+++ helpers/migrate.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+from pprint import pprint
+import os
+
+
+# Source: https://labo301.slack.com/archives/DCCNU4AA2/p1625829587152400
+def rule_minami3():
+    rules = {}
+    with open(os.path.join(os.path.curdir, "./tn3/migration/minami3.txt")) as fd:
+        for n, line in enumerate(fd):
+            if n == 0:
+                continue
+            rule = line.split()
+            if len(rule) == 4:
+                rules[rule[0]] = None
+            if len(rule) > 4:
+                rules[rule[0]] = rule[4]
+    return lambda old_port: rules[old_port]
+
+
+def rule_common():
+    pass
+
+
+def converter(hostname):
+    registry = {
+        "minami3": rule_minami3()
+    }
+    try:
+        return registry[hostname]
+    except KeyError:
+        return rule_common()
+
+
+if __name__ == "__main__":
+    f = converter("minami3")
+    for old in ["ge-0/0/0", "ge-0/0/47", "ge-1/0/0", "ge-1/0/47"]:
+        pprint(f(old))
