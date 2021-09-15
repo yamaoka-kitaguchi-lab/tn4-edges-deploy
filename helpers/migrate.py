a64090acd34c8be1392f79b971b5ed0aa8435207
mi2428 <mi2428782020@gmail.com>
2021-09-15 17:54:52 +0900

    Load migration rule from xlsx file downloaded from Google
    Signed-off-by: mi2428 <mi2428782020@gmail.com>

--- helpers/migrate.py
+++ helpers/migrate.py
@@ -7,10 +7,13 @@ from pprint import pprint
 import os
 import sys
 import gspread
+import openpyxl
 
 # See: https://docs.google.com/spreadsheets/d/11M9m7-C7Ogvuow7F5OG4U--TBk4gwETUcWTZWEJGCOY
 SPREADSHEET_KEY = "11M9m7-C7Ogvuow7F5OG4U--TBk4gwETUcWTZWEJGCOY"
 JSON_KEYFILE_PATH = os.path.join(os.path.dirname(__file__), "../.secrets/googleapi.json")
+
+XLSX_PATH = "./port_migration_rules.xlsx"
 
 
 def open_worksheets(keyfile, sheetkey):
@@ -21,15 +24,27 @@ def open_worksheets(keyfile, sheetkey):
   return workbook.worksheets()
 
 
+def open_xlsx(xlsx_path):
+  book = openpyxl.load_workbook(xlsx_path)
+  return book.worksheets
+
+
+def stringify(c):
+  if c == None: return ""
+  if c == True: return "TRUE"
+  if c == False: return "FALSE"
+  return c
+
+
 def parse_migration_rule(lines):
   rule = {}
   for n, l in enumerate(lines):
-    tn4_port   = l[0]                   # Column A
-    tn3_port   = l[2]                   # Column C
-    tn4_desc   = l[3].replace(" ", "")  # Column D
-    tn4_noshut = l[5] != "down"         # Column F
-    tn4_poe    = l[7] == "TRUE"         # Column H
-    tn4_lag    = l[8]                   # Column I (name of LAG parent)
+    tn4_port   = stringify(l[0])                   # Column A
+    tn3_port   = stringify(l[2])                   # Column C
+    tn4_desc   = stringify(l[3]).replace(" ", "")  # Column D
+    tn4_noshut = stringify(l[5]) != "down"         # Column F
+    tn4_poe    = stringify(l[7]) == "TRUE"         # Column H
+    tn4_lag    = stringify(l[8])                   # Column I (name of LAG parent)
 
     # Header
     if n < 1: continue
@@ -61,19 +76,27 @@ def parse_migration_rule(lines):
   return rule
 
 
-def load(hosts=[]):
-  sheets = open_worksheets(JSON_KEYFILE_PATH, SPREADSHEET_KEY)
+def load(sheets, hosts=[]):
   rules = {}
   for i, sheet in enumerate(sheets):
     tn4_hostname = sheet.title
     if hosts and tn4_hostname not in hosts:
       continue
     print(f"Loading migration rule: {tn4_hostname} (id:{i})")
-    lines = sheet.get_all_values()
+    try:
+      lines = sheet.get_all_values()
+    except AttributeError:
+      lines = [l for l in sheet.values]
     rules[tn4_hostname] = parse_migration_rule(lines)
   return rules
 
 
 if __name__ == "__main__":
-  #pprint(load(hosts=["minami1", "minami2"]))
-  pprint(load())
+  #sheets = open_worksheets(JSON_KEYFILE_PATH, SPREADSHEET_KEY)
+  #pprint(load(sheets, hosts=["minami1", "minami2"]))
+  #pprint(load(sheets))
+  
+  sheets = open_xlsx(XLSX_PATH)
+  pprint(load(sheets, hosts=["minami1", "minami2"]))
+  #pprint(load(sheets))
+  
