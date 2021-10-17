04623bb1ca2207b9d7c5ec88928b2d9cefe264a8
mi2428 <mi2428782020@gmail.com>
2021-10-17 22:50:54 +0900

    develop: Sun 17 Oct 2021 10:50:54 PM JST
    Signed-off-by: mi2428 <mi2428782020@gmail.com>

--- renderer.py
+++ renderer.py
@@ -16,3 +16,16 @@ from netbox import dynamic_inventory
 
 TEMPLATEDIR = os.path.join(CURDIR, "roles")
 
+
+
+def main():
+  parser = argparse.ArgumentParser(description="Rendering config template reflecting NetBox database")
+  parser.add_argument("-t", "--template", dest="tpl_path", help="Path of template (e.g., juniper/overwrite.cfg.j2)")
+  args = parser.parse_args()
+
+  tpl_path = os.path.join(TEMPLATEDIR, args.tpl_path)
+  print(tpl_path)
+
+
+if __name__ == "__main__":
+  main()
