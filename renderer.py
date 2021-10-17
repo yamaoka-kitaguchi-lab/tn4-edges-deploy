4e2935217d864b8fc4adc7ea34ac2d99d27d913c
mi2428 <mi2428782020@gmail.com>
2021-10-17 23:50:20 +0900

    develop: Sun 17 Oct 2021 11:50:20 PM JST
    Signed-off-by: mi2428 <mi2428782020@gmail.com>

--- renderer.py
+++ renderer.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 
-from jinja2 import Environment
-from jinja2 import FileSystemLoader
 from pprint import pprint
 import argparse
 import jinja2
 import os
 import sys
+import time
 
 CURDIR = os.path.dirname(os.path.abspath(__file__))
 INVENTORYDIR = os.path.join(CURDIR, "inventories/production")
@@ -14,17 +13,55 @@ sys.path.append(INVENTORYDIR)
 
 from netbox import dynamic_inventory
 
-TEMPLATEDIR = os.path.join(CURDIR, "roles")
 
+def load_inventories():
+  start_at = time.time()
+  print("Loading inventories from NetBox, this may take a while...", end=" ", flush=True)
+  inventories = dynamic_inventory()
+  elapsed_time = round(time.time() - start_at, 1)
+  print(f"completed successfully ({elapsed_time}sec)", flush=True)
+  return inventories
+
+
+def render_templates(tpl_path, device_role, inventories, trim_blocks=False):
+  loader_base = os.path.join(CURDIR, os.path.dirname(tpl_path))
+  tpl_name = os.path.basename(tpl_path)
+  env = jinja2.Environment(loader=jinja2.FileSystemLoader(loader_base), trim_blocks=trim_blocks)
+
+  try:
+    template = env.get_template(tpl_name)
+  except jinja2.exceptions.TemplateNotFound:
+    print(f"No such template: {tpl_path}", file=sys.stderr)
+    sys.exit(1)
+
+  try:
+    hosts = inventories[device_role]["hosts"]
+  except KeyError:
+    print(f"No such device role: {device_role}", file=sys.stderr)
+    sys.exit(2)
+
+  results = {}
+  for host in hosts:
+    params = inventories["_meta"]["hostvars"][host]
+    results[host] = template.render(params)
+
+  return results
 
 
 def main():
-  parser = argparse.ArgumentParser(description="Rendering config template reflecting NetBox database")
-  parser.add_argument("-t", "--template", dest="tpl_path", help="Path of template (e.g., juniper/overwrite.cfg.j2)")
+  parser = argparse.ArgumentParser(description="rendering config template reflecting NetBox database")
+  parser.add_argument("-t", "--template", required=True, dest="PATH", help="path of the template from (e.g., ./roles/juniper/templates/overwrite.cfg.j2)")
+  parser.add_argument("-r", "--device-role", required=True, dest="ROLE", help="device role (e.g., edge-sw)")
   args = parser.parse_args()
 
-  tpl_path = os.path.join(TEMPLATEDIR, args.tpl_path)
-  print(tpl_path)
+  tpl_path = args.SUBPATH.strip("/")
+  device_role = args.ROLE.upper()
+  inventories = load_inventories()
+
+  results = render_templates(tpl_path, device_role, inventories)
+  for hostname, result in results.items():
+    print("\n".join([":"*10, hostname, ":"*10]))
+    print(result, end="\n")
 
 
 if __name__ == "__main__":
